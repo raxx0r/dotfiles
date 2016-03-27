a47254b2d18ea7c3b662e322d30ae80dbe2088bf
Oscar Johnson <o.johnsson@live.se>
2016-03-27 14:40:04 +0200

    Added previously ignored folder

---
+++ .install/brew.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# give me what I need, brew!
+
+# development tools
+brew install git
+brew install node 
+brew install brew-cask
+
+brew cask install skype
+brew cask install spotify
+brew cask install vlc
+brew cask install google-chrome
+brew cask install utorrent
+brew cask install steam
+brew cask install sublime-text
+brew cask install dropbox
+#brew cask install java ??
+#brew cask install microsoft-office??

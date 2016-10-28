39e4adf36a0257cca4d053fa58868dfaa3ce41e9
Oscar Johnson <o.johnsson@live.se>
2016-10-28 14:10:08 +0200

    Start off automatic mapping of caps->esc

--- setup.sh
+++ setup.sh
@@ -1,1 +1,10 @@
 defaults write NSGlobalDomain KeyRepeat -int 0
+
+
+# Remap caps to escape
+
+# 1. install seil (brew?)
+# 2. set no action in system settings 
+# 3. set mapping in seil
+# read more here: https://pqrs.org/osx/karabiner/seil.html.en
+/Applications/Seil.app/Contents/Library/bin/seil
